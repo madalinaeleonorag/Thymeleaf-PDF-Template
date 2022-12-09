edeb7008aec3044df3f3f97906afca7d37ca51a8
Cristina <antohe_cristina@yahoo.co.uk>
2022-12-09 19:09:30 +0200

    Added missing file

---
+++ src/main/java/test/DocumentGeneration/ThymeleafPdfTemplateApplication.java
@@ -0,0 +1,13 @@
+package test.DocumentGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPdfTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPdfTemplateApplication.class, args);
+	}
+
+}
